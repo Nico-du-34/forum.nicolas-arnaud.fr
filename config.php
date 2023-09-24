a8a9533e8f5f7b9e46780ae5cfaea16e00ff61a0
Nico-du-34 <lola@titinico.fr>
2023-09-24 16:42:18 +0000

    initial project version 1.0.0

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'forum',
+    'username' => 'forum',
+    'password' => 'I83u~x3v0',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'https://forum.nicolas-arnaud.fr',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
